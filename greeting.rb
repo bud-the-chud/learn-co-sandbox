f005d5b55589255e5887b124c337a1bd60488630
s-satsangi <62524673+s-satsangi@users.noreply.github.com>
2020-09-09 14:04:00 -0400

    Update greeting.rb

--- greeting.rb
+++ greeting.rb
@@ -1,3 +1,4 @@
+#Why does this stop executing after line 9, and not 13????
 require 'pry'
 def greeting
   puts "Henlo Buddie"
@@ -5,12 +6,12 @@ end
 
 
 def say_it_x_five whatev=greeting
+  whatev
+  whatev
+  whatev
+  whatev
+  whatev
   binding.pry
-  whatev
-  whatev
-  whatev
-  whatev
-  whatev
 end
 
 say_it_x_five
