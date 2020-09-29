37fd6581bd505404951652f101c6da0f13cd12a5
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-29 06:08:22 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 #Why does this stop executing after line 9, and not 13????
+=======
+>>>>>>> 7bd279049cf893ab4848423cf5b7a91a17873cf4
 require 'pry'
 def greeting
   puts "Henlo Buddie"
@@ -6,14 +9,25 @@ end
 
 
 def say_it_x_five whatev=greeting
-  whatev
-  whatev
-  whatev
-  whatev
-  whatev
+<<<<<<< HEAD
+=======
   binding.pry
+>>>>>>> 7bd279049cf893ab4848423cf5b7a91a17873cf4
+  whatev
+  whatev
+  whatev
+  whatev
+  whatev
+<<<<<<< HEAD
+  binding.pry
+=======
+>>>>>>> 7bd279049cf893ab4848423cf5b7a91a17873cf4
 end
 
 say_it_x_five
 
+<<<<<<< HEAD
 say_it_x_five rando
+=======
+say_it_x_five rando
+>>>>>>> 7bd279049cf893ab4848423cf5b7a91a17873cf4
